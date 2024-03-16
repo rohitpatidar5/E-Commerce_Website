9df40691afe006aa79874ba564dd1cfb1c9dd1e8
Rohit Patidar <rohitpatidar583@gmail.com>
2024-03-16 17:26:45 +0530

    complete nav-cart-count functionality

--- FRONTEND/src/Context/ShopContext.jsx
+++ FRONTEND/src/Context/ShopContext.jsx
@@ -37,7 +37,19 @@ const ShopContexProvider = (props) => {
         return totalAmount;
     }
     
-    const contextValue = { getTotalCartAmount, all_product, cartItems,addToCart,removeFromCart };
+const getTotalCartItems =() =>{
+    let totalItem = 0;
+    for(const item in cartItems)
+    {
+        if(cartItems[item]>0)
+        {
+            totalItem+= cartItems[item];
+        }
+    }
+    return totalItem;
+}
+
+    const contextValue = { getTotalCartItems, getTotalCartAmount, all_product, cartItems,addToCart,removeFromCart };
 
     return (
         <ShopContext.Provider value={contextValue}>
